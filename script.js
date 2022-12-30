ca365ef93af3e09ac05ec02a70cb38bb5f63e6f3
José Leite <jf.leiteee@gmail.com>
2022-12-30 18:21:24 -0300

    alternativas certas e erradas

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
 const btn = document.querySelectorAll(".btn");
 const answer = document.querySelectorAll(".answer");
-let correct = 0;
 
 for(let i = 0; i <= 16; i++) {
     btn[i].addEventListener("click", function (e) {
-
-        let respostas = ["Alvarez Cabral", "Yuri Gagarin", "206", "Mercúrio" ]
-
-        verificar();
-
-        function verificar() { 
-            if(!btn[i].innerHTML){
-                return;
-            }
-            else {
-            for(let p = 0; p <= 3; p++) {
-                if(btn[i].innerHTML != respostas[p]){
-                    btn[i].classList.add("wrong");
-                }
-                else {
-                    btn[i].classList.add("correct");
-                    return;
-                }
-            }
-            }
+        console.log(btn[i].dataset.resposta)
+        
+        // let respostas = ["Alvarez Cabral", "Yuri Gagarin", "206", "Mercúrio"];
+        if(btn[i].dataset.resposta == "correta") {
+            btn[i].classList.add("correct");
+        }
+        else {
+            btn[i].classList.add("wrong");
         }
     })
 }
