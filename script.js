4736f158e39eb1ee3a9b5cf0f6a428eeb90de02c
José Leite <jf.leiteee@gmail.com>
2022-12-30 10:45:16 -0300

    sistema de acerto QUASE pronto

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("oi")
+const btn = document.querySelectorAll(".btn");
+const answer = document.querySelectorAll(".answer");
+let correct = 0;
+
+for(let i = 0; i <= 16; i++) {
+    btn[i].addEventListener("click", function (e) {
+
+        let respostas = ["Alvarez Cabral", "Yuri Gagarin", "206", "Mercúrio" ]
+
+        verificar();
+
+        function verificar() { 
+            if(!btn[i].innerHTML){
+                return;
+            }
+            else {
+            for(let p = 0; p <= 3; p++) {
+                if(btn[i].innerHTML != respostas[p]){
+                    btn[i].classList.add("wrong");
+                }
+                else {
+                    btn[i].classList.add("correct");
+                    return;
+                }
+            }
+            }
+        }
+    })
+}
