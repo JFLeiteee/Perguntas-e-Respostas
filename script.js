5271763489cb9b46d93b6937e839ca42b51f90e6
José Leite <jf.leiteee@gmail.com>
2022-12-31 10:48:45 -0300

    resultado de acertos e erros

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const btn = document.querySelectorAll(".btn");
 const answer = document.querySelectorAll(".answer");
+const correct = document.querySelector(".corrects");
+const result = document.querySelector(".result");
+const wrong = document.querySelector(".wrongs");
+let correto = 0;
+let errado = 0;
 
 for(let i = 0; i <= 16; i++) {
     btn[i].addEventListener("click", function (e) {
@@ -8,9 +13,16 @@ for(let i = 0; i <= 16; i++) {
         // let respostas = ["Alvarez Cabral", "Yuri Gagarin", "206", "Mercúrio"];
         if(btn[i].dataset.resposta == "correta") {
             btn[i].classList.add("correct");
+            btn[i].style.cursor = "not-allowed"
+            correto += 1;
+            result.getElementsByTagName("span")[0].innerHTML = correto;
         }
         else {
             btn[i].classList.add("wrong");
+            btn[i].style.cursor = "not-allowed"
+            errado += 1;
+            result.getElementsByTagName("span")[1].innerHTML = errado;
         }
+
     })
 }
